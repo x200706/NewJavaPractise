48f64fb111ca9a387e5f569666950a95770dde22
x200706 <99391710+x200706@users.noreply.github.com>
2023-12-08 08:56:06 +0000

    add a stream test

--- StreamCamp2.java
+++ StreamCamp2.java
@@ -16,6 +16,11 @@ class StreamCamp2 {
         .filter(result -> result.score() > 80)
         .collect(Collectors.toList());
     query.forEach(result -> System.out.println("表揚考試分數大於八十分的寶寶" + result.name()));
+
+    // 後來我尋思一件奇怪的事情，如果只是要sout這樣寫就好了吧
+    studentExamResults.stream()
+        .filter(result -> result.score() > 80)
+        .forEach(result -> System.out.println("表揚考試分數大於八十分的寶寶" + result.name()));
   }
 
 }
